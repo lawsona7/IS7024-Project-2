58116a3a8a77c6c3c60f3c2cecdcb593c5f70d5f
lawsona7 <lawsona7@mail.uc.edu>
2016-10-31 22:10:04 -0400

    Test commit on Branch
    First commit on the branch

--- IS7024 Project 2/Form1.Designer.cs
+++ IS7024 Project 2/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FileSelection = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.fileLocation = new System.Windows.Forms.TextBox();
+            this.xmlView = new System.Windows.Forms.RichTextBox();
+            this.queryResults = new System.Windows.Forms.RichTextBox();
+            this.xpathQuery = new System.Windows.Forms.TextBox();
+            this.xpathButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FileSelection
+            // 
+            this.FileSelection.Location = new System.Drawing.Point(85, 40);
+            this.FileSelection.Name = "FileSelection";
+            this.FileSelection.Size = new System.Drawing.Size(115, 26);
+            this.FileSelection.TabIndex = 0;
+            this.FileSelection.Text = "Select File";
+            this.FileSelection.UseVisualStyleBackColor = true;
+            this.FileSelection.Click += new System.EventHandler(this.FileSelection_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(85, 72);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(571, 26);
+            this.fileName.TabIndex = 1;
+            // 
+            // fileLocation
+            // 
+            this.fileLocation.Location = new System.Drawing.Point(206, 40);
+            this.fileLocation.Name = "fileLocation";
+            this.fileLocation.Size = new System.Drawing.Size(450, 26);
+            this.fileLocation.TabIndex = 2;
+            // 
+            // xmlView
+            // 
+            this.xmlView.Location = new System.Drawing.Point(85, 126);
+            this.xmlView.Name = "xmlView";
+            this.xmlView.Size = new System.Drawing.Size(571, 698);
+            this.xmlView.TabIndex = 4;
+            this.xmlView.Text = "";
+            // 
+            // queryResults
+            // 
+            this.queryResults.Location = new System.Drawing.Point(867, 126);
+            this.queryResults.Name = "queryResults";
+            this.queryResults.Size = new System.Drawing.Size(624, 698);
+            this.queryResults.TabIndex = 5;
+            this.queryResults.Text = "";
+            // 
+            // xpathQuery
+            // 
+            this.xpathQuery.Location = new System.Drawing.Point(992, 71);
+            this.xpathQuery.Name = "xpathQuery";
+            this.xpathQuery.Size = new System.Drawing.Size(499, 26);
+            this.xpathQuery.TabIndex = 6;
+            // 
+            // xpathButton
+            // 
+            this.xpathButton.Location = new System.Drawing.Point(867, 70);
+            this.xpathButton.Name = "xpathButton";
+            this.xpathButton.Size = new System.Drawing.Size(116, 27);
+            this.xpathButton.TabIndex = 7;
+            this.xpathButton.Text = "Search";
+            this.xpathButton.UseVisualStyleBackColor = true;
+            this.xpathButton.Click += new System.EventHandler(this.xpathButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1830, 836);
+            this.Controls.Add(this.xpathButton);
+            this.Controls.Add(this.xpathQuery);
+            this.Controls.Add(this.queryResults);
+            this.Controls.Add(this.xmlView);
+            this.Controls.Add(this.fileLocation);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.FileSelection);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button FileSelection;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.TextBox fileLocation;
+        private System.Windows.Forms.RichTextBox xmlView;
+        private System.Windows.Forms.RichTextBox queryResults;
+        private System.Windows.Forms.TextBox xpathQuery;
+        private System.Windows.Forms.Button xpathButton;
     }
 }
 
--- IS7024 Project 2/Form1.cs
+++ IS7024 Project 2/Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows.Forms;
+using System.Xml;
+
 
 namespace IS7024_Project_2
 {
@@ -15,6 +18,43 @@ namespace IS7024_Project_2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void FileSelection_Click(object sender, EventArgs e)
+        {
+            Stream xmlStream;
+            ofd.Filter = "XML|*.xml";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if ((xmlStream = ofd.OpenFile()) !=null)
+                {
+                    fileName.Text = ofd.FileName;
+                    fileLocation.Text = ofd.SafeFileName;
+                    
+                    string xmlName = ofd.FileName;
+                    string xmlText = File.ReadAllText(xmlName);
+                    xmlView.Text = xmlText;
+                }
+
+            }
+        }
+
+        private void xpathButton_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                XmlDocument xDoc = new XmlDocument();
+                xDoc.Load(ofd.FileName);
+                XmlNodeList nodeList;
+                XmlNode root = xDoc.DocumentElement;
+                string search = xpathQuery.Text;
+                nodeList = root.SelectNodes(search);
+                
+                queryResults.Text = nodeList;
+            }
         }
     }
 }
