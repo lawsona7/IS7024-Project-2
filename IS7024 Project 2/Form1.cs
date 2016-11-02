f10be5c1f4cdc6eb1e3043e8b49ec7c9b368979a
lawsona7 <lawsona7@mail.uc.edu>
2016-11-01 20:19:40 -0400

    Change 1
    Change the outlay

--- IS7024 Project 2/Form1.cs
+++ IS7024 Project 2/Form1.cs
@@ -9,7 +9,9 @@ using System.Text;
 using System.Security.Cryptography;
 using System.Windows.Forms;
 using System.Xml;
-
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Xml.XPath;
 
 namespace IS7024_Project_2
 {
@@ -21,40 +23,37 @@ namespace IS7024_Project_2
         }
 
         OpenFileDialog ofd = new OpenFileDialog();
+        XmlDocument xDoc = new XmlDocument();
 
         private void FileSelection_Click(object sender, EventArgs e)
         {
             Stream xmlStream;
             ofd.Filter = "XML|*.xml";
-
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                if ((xmlStream = ofd.OpenFile()) !=null)
+                if ((xmlStream = ofd.OpenFile()) != null)
                 {
-                    fileName.Text = ofd.FileName;
-                    fileLocation.Text = ofd.SafeFileName;
-                    
-                    string xmlName = ofd.FileName;
-                    string xmlText = File.ReadAllText(xmlName);
-                    xmlView.Text = xmlText;
-                }
 
+
+                        //assign text to text boxes
+                        fileName.Text = ofd.FileName;
+                        fileLocation.Text = ofd.SafeFileName;
+
+
+
+                        //read in text from xml file
+                        string xmlName = ofd.FileName;
+                        string xmlText = File.ReadAllText(xmlName);
+                        xmlView.Text = xmlText;
+                }
             }
         }
 
         private void xpathButton_Click(object sender, EventArgs e)
         {
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                XmlDocument xDoc = new XmlDocument();
-                xDoc.Load(ofd.FileName);
-                XmlNodeList nodeList;
-                XmlNode root = xDoc.DocumentElement;
-                string search = xpathQuery.Text;
-                nodeList = root.SelectNodes(search);
-                
-                queryResults.Text = nodeList;
-            }
+            
+            XmlDocument 
+            queryResults.Text = xDoc.DocumentElement.OuterXml;
         }
     }
 }
